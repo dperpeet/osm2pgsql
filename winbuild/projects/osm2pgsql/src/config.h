6af7c2dedb5936fdd37b23552e0fc8f57927b495
dominik <dominik@perpeet.eu>
2015-11-29 22:34:48 +0100

    windows: build osm2pgsql with msvc

---
+++ winbuild/projects/osm2pgsql/src/config.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDED_CONFIG
+#define INCLUDED_CONFIG
+
+#define OSM2PGSQL_DATADIR "D:/maps"
+#define VERSION "0.88.x (ec7082af44d375aa6e5ee5ebd6f524b35afd5265)"
+#define OSM2PGSQL_EXTRAINFO "Windows build https://github.com/dperpeet/osm2pgsql"
+
+#ifdef _WIN32
+#	define ssize_t SSIZE_T
+#endif
+
+#endif
